0a74600a6b33b0dfaa7882de4218c1de9076ed67
zackglenen <w0286403@nscc.ca>
2023-01-30 14:31:41 -0400

    x

--- Assignments/Assignment2/main.js
+++ Assignments/Assignment2/main.js
@@ -3,7 +3,16 @@
     fetch('https://api.magicthegathering.io/v1/cards')
     .then(response => response.json())
     .then(data => {
-        console.log(JSON.stringify(data))
+        let outputHtml = "<ul>"
+
+        for (let i = 0; i < 3; i++) {
+            outputHtml += "<li>"
+            outputHtml += JSON.stringify(data.cards[i])
+            outputHtml += "</li>"
+            
+        }
+        outputHtml += "</ul>"
+        document.write(outputHtml)
         
     });
     
