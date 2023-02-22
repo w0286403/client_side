e8e385b48e0ea47ffad700dc421c02d085f849b0
zackglenen <w0286403@nscc.ca>
2023-02-21 22:05:26 -0400

    vv

--- Assignments/Assignment3/partC/buses/main.js
+++ Assignments/Assignment3/partC/buses/main.js
@@ -1,26 +1,70 @@
 (function(){
 
-    fetch('https://prog2700.onrender.com/hrmbuses')
-    .then((response) => response.json())
-    .then((data) => {
-        data = filterResult(data)
-        console.log(data)
-    })
+        const map = L.map('theMap').setView([44.650627, -63.597140], 14);
+        const busIcon = L.icon({
+           iconUrl: 'bus.png',
+           iconSize: [25,25]
+       });
+  
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+               maxZoom: 18,
+               minZoom: 10,
+               attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+  
+    
+        fetch('https://prog2700.onrender.com/hrmbuses')
+        .then((response) => response.json())
+        .then((data) => {
 
-    const filterResult = (json) => {
-        return json.entity.filter((bus)=> bus.vehicle.trip.routeId <= 10)
+            console.log(data)
+            let geoJson = transformToGeoJson(data);
+            console.log(geoJson)
+            L.geoJSON(geoJson,{
+
+                pointToLayer: function (feature, latlng) {//SOURCE:https://gis.stackexchange.com/questions/245583/rotate-point-markers-geojson-leaflet
+                    return L.marker(latlng, {icon: busIcon, rotationAngle: feature.properties.rotationAngle})
+                },
+                onEachFeature: onEachFeature
+            }).addTo(map);
+
+        })
+
+    const onEachFeature = (feature, layer) => {//https://leafletjs.com/examples/geojson/
+        if (feature.properties && feature.properties.popupContent) {
+            layer.bindPopup(feature.properties.popupContent);
+        }
     }
+    
 
-    //create map in leaflet and tie it to the div called 'theMap'
-    // const map = L.map('theMap').setView([44.650627, -63.597140], 14);
+    const transformToGeoJson = (json) => {
 
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    //     }).addTo(map);
+        let geoJson = {
+            "type": "FeatureCollection",
+            "features": [
+            ]
+        }
 
-    // L.marker([44.650690, -63.596537]).addTo(map)
-    //     .bindPopup('This is a sample popup. You can put any html structure in this including extra bus data. You can also swap this icon out for a custom icon. A png file has been provided for you to use if you wish.')
-    //     .openPopup();
+        json.entity.filter((bus)=> {
+            if (bus.vehicle.trip.routeId <= 10){
+                geoJson.features.push({                 
+                    "type": "Feature",
+                    "properties": {
+                        "name": bus.id,
+                        "popupContent": "ROUTE: " + bus.vehicle.trip.routeId,
+                        "rotationAngle": bus.vehicle.position.bearing
+                    },
+                    "geometry": {
+                        "coordinates": [
+                            bus.vehicle.position.longitude,
+                            bus.vehicle.position.latitude
+                        ],
+                        "type": "Point"
+                    }
+                })
+            }
+        })
 
-
+        return geoJson;
+    }
 })()
