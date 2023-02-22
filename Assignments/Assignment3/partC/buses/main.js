106cf904a72aa56460ed8fa88f30932a13d38cf1
zackglenen <w0286403@nscc.ca>
2023-02-22 00:04:35 -0400

    WORKING

--- Assignments/Assignment3/partC/buses/main.js
+++ Assignments/Assignment3/partC/buses/main.js
@@ -1,34 +1,38 @@
 (function(){
 
-        const map = L.map('theMap').setView([44.650627, -63.597140], 14);
-        const busIcon = L.icon({
-           iconUrl: 'bus.png',
-           iconSize: [25,25]
-       });
-  
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-               maxZoom: 18,
-               minZoom: 10,
-               attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
-  
-    
+    const map = L.map('theMap').setView([44.650627, -63.597140], 14);
+    const busIcon = L.icon({
+    iconUrl: 'bus.png',
+    iconSize: [30,40],
+    iconAnchor: [15,20]
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        minZoom: 10,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+      
+    const getData = () => {
+        let geo;
         fetch('https://prog2700.onrender.com/hrmbuses')
-        .then((response) => response.json())
+        .then((response)=>response.json())
         .then((data) => {
-
-            console.log(data)
             let geoJson = transformToGeoJson(data);
-            console.log(geoJson)
-            L.geoJSON(geoJson,{
-
+            geo =  L.geoJSON(geoJson,{
                 pointToLayer: function (feature, latlng) {//SOURCE:https://gis.stackexchange.com/questions/245583/rotate-point-markers-geojson-leaflet
                     return L.marker(latlng, {icon: busIcon, rotationAngle: feature.properties.rotationAngle})
                 },
                 onEachFeature: onEachFeature
-            }).addTo(map);
+            }).addTo(map)
 
         })
+        (setInterval(()=>{
+            map.removeLayer(geo)
+            getData()
+        },4000))
+    }
+
 
     const onEachFeature = (feature, layer) => {//https://leafletjs.com/examples/geojson/
         if (feature.properties && feature.properties.popupContent) {
@@ -50,8 +54,8 @@
                 geoJson.features.push({                 
                     "type": "Feature",
                     "properties": {
-                        "name": bus.id,
-                        "popupContent": "ROUTE: " + bus.vehicle.trip.routeId,
+                        "popupContent": "ROUTE: " + bus.vehicle.trip.routeId +
+                        "<br>ID: " +  bus.id,
                         "rotationAngle": bus.vehicle.position.bearing
                     },
                     "geometry": {
@@ -67,4 +71,6 @@
 
         return geoJson;
     }
+    getData();
+    
 })()
